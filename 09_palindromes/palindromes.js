608081451fdbb5245191ab526b3d9a5a4baaaa2e
11aciek <10337645+11aciek@users.noreply.github.com>
2024-11-24 14:58:03 +0100

    Solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const punctation = [".", "!", "?", ";", ",", ":", " ", "'"];
+  const test = word.toLowerCase().split("")
+				.filter(character => !punctation.includes(character))
+				.join("");
+  return test == test.split("").reverse().join("");
 };
 
 // Do not edit below this line
