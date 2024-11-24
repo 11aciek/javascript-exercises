e0dc498d9f940481d66ca70a671f4cd084f2576b
11aciek <10337645+11aciek@users.noreply.github.com>
2024-11-24 15:38:54 +0100

    Solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(limit) {
+  if (limit < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  while (limit > 0) {
+	[a, b] = [b, a + b];
+	limit--;
+  }
+  return a;
 };
 
 // Do not edit below this line
