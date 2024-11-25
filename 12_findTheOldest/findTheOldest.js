0252941159fe8aba49e5356a0bf639ff60261102
11aciek <10337645+11aciek@users.noreply.github.com>
2024-11-25 22:45:24 +0100

    Solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+  const currYear = new Date().getFullYear();
+  return people.reduce((oldest, next) => 
+	((next.yearOfDeath || currYear) - next.yearOfBirth) > ((oldest.yearOfDeath || currYear) - oldest.yearOfBirth) ?
+	next : oldest);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
